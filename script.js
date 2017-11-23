e055ac57ad568439f4423b0133151553b8f6b564
Connor French <cbfrench@calpoly.edu>
2017-11-22 17:12:25 -0800

    Version 2.0 overhaul

--- script.js
+++ script.js
@@ -1,317 +1,137 @@
-//global variables
 var theme = 0;
-var redColor = 0;
-var greenColor = 100;
-var blueColor = 50;
-var redAdd = 3;
-var greenAdd = 2;
-var blueAdd = 1;
-var redSubtract = 1;
-var greenSubtract = 2;
-var blueSubtract = 3;
-var redCountUp = false;
-var greenCountUp = false;
-var blueCountUp = false;
-var authorization = false;
-var time = 150;
-var trackOne = new Audio("forest.mp3");
-var trackTwo = new Audio("beach.mp3");
-var trackThree = new Audio("meadow.mp3");
-var trackFour = new Audio("whiteNoise.mp3");
-var trackFive = new Audio("sun.mp3");
-var muted = false;
-var code = false;
-var full = 0;
+var themes = ["forest", "beach", "meadow", "white noise", "sun"];
+var running = 0;
+var forestAudio = new Audio("forest.mp3");
+var beachAudio = new Audio("beach.mp3");
+var meadowAudio = new Audio("meadow.mp3");
+var noiseAudio = new Audio("whiteNoise.mp3");
+var sunAudio = new Audio("sun.mp3");
+var audioTrack = forestAudio;
+var tracks = [forestAudio, beachAudio, meadowAudio, noiseAudio, sunAudio];
+var muted = 0;
+var colors = ["#036633", "#643200", "#003264", "#000000", "#00aa00"];
+var forestInc = [0, 1, 0];
+var beachInc = [2, 3, 1];
+var meadowInc = [0, 2, 1];
+var noiseInc = [1, 1, 1];
+var sunInc = [5, 1, 0];
+var incs = [forestInc, beachInc, meadowInc, noiseInc, sunInc];
+var curInc = forestInc;
+var speeds = [50, 50, 50, 25, 25];
+var curSpeed = speeds[0];
 
-
-//registers the press of the top button and sets a boolean to represent it
-function colorButton()
-{
-    musicSelector();
-    if(authorization)
-    {
-        authorization = false;
+function toggleBegin(){
+    var begin = document.getElementById("begin");
+    if(begin.innerHTML === "begin"){
+        return "stop";
     }
-    else
-    {
-        authorization = true;
-    }
+    return "begin";
 }
 
-
-//sets up the colors and text that appear during theme selection
-function themeSelect()
-{
-    var redFixed = colorFixer(redColor.toString(16), "red");
-    var greenFixed = colorFixer(greenColor.toString(16), "green");
-    var blueFixed = colorFixer(blueColor.toString(16), "blue");
-    var themeOne = "#" + redFixed + greenFixed + blueFixed;
-    var themeTwo = "#" + greenFixed + blueFixed + redFixed;
-    var themeThree = "#" + redFixed + blueFixed + greenFixed;
-    var themeFour = "#" + redFixed + redFixed + redFixed;
-    var themeFive = "#" + redFixed + "aa" + "00";
-    if(!authorization)
-    {                
-        if(theme === 4)
-        {
-            theme = 0;
-            document.body.style.backgroundColor = themeOne;
-            document.getElementById("color").innerHTML = "forest";
-        }
-        else if(theme === 0)
-        {
-            theme = 1;
-            document.body.style.backgroundColor = themeTwo;
-            document.getElementById("color").innerHTML = "beach";
-        }
-        else if(theme === 1)
-        {
-            theme = 2;
-            document.body.style.backgroundColor = themeThree;
-            document.getElementById("color").innerHTML = "meadow";
-        }
-        else if(theme === 2)
-        {
-            theme = 3;
-            document.body.style.backgroundColor = themeFour;
-            document.getElementById("color").innerHTML = "white noise";
-        }
-        else
-        {
-            theme = 4;
-            document.body.style.backgroundColor = themeFive;
-            document.getElementById("color").innerHTML = "sun";
-        }
-    }
+function toggleTheme(){
+    return;
 }
 
-
-//changes the color while running
-function colors()
-{   
-    if(!redCountUp)
-    {
-        redColor += redAdd;
+function toggleMute(){
+    var mute = document.getElementById("mute");
+    if(mute.innerHTML === "mute"){
+        muted = 1;
+        return "unmute";
     }
-    if(!greenCountUp)
-    {
-        greenColor += greenAdd;
-    }
-    if(!blueCountUp)
-    {
-        blueColor += blueAdd;
-    }
-    if(redCountUp)
-    {
-        redColor -= redSubtract;
-    }
-    if(greenCountUp)
-    {
-        greenColor -= greenSubtract;
-    }
-    if(blueCountUp)
-    {
-        blueColor -= blueSubtract;
-    }
-    var redFixed = colorFixer(redColor.toString(16), "red");
-    var greenFixed = colorFixer(greenColor.toString(16), "green");
-    var blueFixed = colorFixer(blueColor.toString(16), "blue");
-    if(theme === 0)
-    {
-        document.body.style.backgroundColor = "#" + redFixed + greenFixed + blueFixed;
-        document.getElementById("color").innerHTML = "#" + redFixed + greenFixed + blueFixed;
-    }
-    if(theme === 1)
-    {
-        document.body.style.backgroundColor = "#" + greenFixed + blueFixed + redFixed;
-        document.getElementById("color").innerHTML = "#" + greenFixed + blueFixed + redFixed;
-    }
-    if(theme === 2)
-    {
-        document.body.style.backgroundColor = "#" + redFixed + blueFixed + greenFixed;
-        document.getElementById("color").innerHTML = "#" + redFixed + blueFixed + greenFixed;
-    }
-    if(theme === 3)
-    {
-        document.body.style.backgroundColor = "#" + redFixed + redFixed + redFixed;
-        document.getElementById("color").innerHTML = "#" + redFixed + redFixed + redFixed;
-    }
-    if(theme === 4)
-    {
-        document.body.style.backgroundColor = "#" + redFixed + "aa" + "00";
-        document.getElementById("color").innerHTML = "#" + redFixed + "aa" + "00";
-    }
+    muted = 0;
+    return "mute";
 }
 
-
-//main function
-window.setInterval(
-function start() {
-    if(authorization)
-    {
-        colors();
-        document.getElementById("begin").innerHTML = "stop";
-        document.getElementById("theme").style.display = "none";
-        document.getElementById("mute").style.display = "none";
-        document.getElementById("version").style.display = "none";
-        document.getElementById("copyright").style.display = "none";
-        document.getElementById("fullScreen").style.display = "none";
-        document.getElementById("begin").style.marginBottom = "30em";
-    }
-    if(!authorization)
-    {
-        document.getElementById("begin").innerHTML = "begin";
-        document.getElementById("theme").style.display = "flex";
-        document.getElementById("mute").style.display = "inline";
-        document.getElementById("version").style.display = "flex";
-        document.getElementById("copyright").style.display = "flex";
-        document.getElementById("version").style.justifyContent = "center";
-        document.getElementById("copyright").style.justifyContent = "center";
-        document.getElementById("fullScreen").style.display = "inline";
-        document.getElementById("begin").style.marginBottom = "0em";
-    }
-}, time);
-
-
-//fixes all issues with the numbers created to allow conversion to hexadecimal and determines whether the colors should start ascending or descending 
-function colorFixer(input, colorChosen)
-{
-    if(input === "0" && colorChosen === "red")
-    {
-        redCountUp = false;
-        return "0" + input;
-    }
-    if(input === "0" && colorChosen === "green")
-    {
-        greenCountUp = false;
-        return "0" + input;
-    }
-    if(input === "0" && colorChosen === "blue")
-    {
-        blueCountUp = false;
-        return "0" + input;
-    }
-    if(input === "ff" && colorChosen === "red")
-    {
-        redCountUp = true;
-        return input;
-    }
-    if(input === "fe" && colorChosen === "green")
-    {
-        greenCountUp = true;
-        return input;
-    }
-    if(input === "ff" && colorChosen === "blue")
-    {
-        blueCountUp = true;
-        return input;
-    }
-    else if(input.length < 2)
-    {
-        return "0" + input;
-    }
-    else
-    {
-        return input;
-    }
-}
-
-
-//plays, loops, pauses, and determines which audio should be played 
-function musicSelector()
-{
-    if(authorization || muted)
-    {
-        if(theme === 0)
-        {
-            trackOne.loop = false;
-            trackOne.pause();
+function audio(){
+    if(!running){
+        if(!muted){
+            audioTrack.play();
+            audioTrack.loop = true;
         }
-        if(theme === 1)
-        {
-            trackTwo.loop = false;
-            trackTwo.pause();
-        }
-        if(theme === 2)
-        {
-            trackThree.loop = false;
-            trackThree.pause();
-        }
-        if(theme === 3)
-        {
-            trackFour.loop = false;
-            trackFour.pause();
-        }
-        if(theme === 4)
-        {
-            trackFive.loop = false;
-            trackFive.pause();
-        }
-    }
-    else
-    {
-        if(theme === 0 && !muted)
-        {
-            trackOne.play();
-            trackOne.loop = true;
-        }
-        if(theme === 1 && !muted)
-        {
-            trackTwo.play();
-            trackTwo.loop = true;
-        }
-        if(theme === 2 && !muted)
-        {
-            trackThree.play();
-            trackThree.loop = true;
-        }
-        if(theme === 3 && !muted)
-        {
-            trackFour.play();
-            trackFour.loop = true;
-        }
-        if(theme === 4 && !muted)
-        {
-            trackFive.play();
-            trackFive.loop = true;
-        }
-    }
-}
-
-
-//helper function that mutes the current track playing
-function muter()
-{
-    if(muted)
-    {
-        document.getElementById("mute").innerHTML = "mute";
-        muted = false;
-    }
-    else
-    {
-        document.getElementById("mute").innerHTML = "unmute";
-        muted = true;
-    }
-}
-
-function screen(){
-    if(full != -1){
-        fillScreen();
+        running = 1;    
     }
     else{
-        exitFullScreen();
+        audioTrack.loop = false;
+        audioTrack.pause();
+        running = 0;
     }
 }
 
-function exitFullScreen(){
-    full = 0;
-    document.webkitExitFullScreen();
+function checkColoring(red, green, blue){
+    var r = parseInt(red, 16);
+    var g = parseInt(green, 16);
+    var b = parseInt(blue, 16);
+    if(r < 1 || r > 254){
+        curInc[0] *= -1;
+    }
+    if(g < 1 || g > 254){
+        curInc[1] *= -1;
+    }
+    if(b < 1 || b > 254){
+        curInc[2] *= -1;
+    }
 }
 
-
-//makes the page take up the entire screen
-function fillScreen()
-{
-    full = 1;
-    document.body.webkitRequestFullScreen();
+function fixString(color){
+    if(color.length === 1){
+        return "0" + color;
+    }
+    return color;
 }
+
+$(function(){
+    $("#begin").click(function(){
+        var text = toggleBegin();
+        $("#begin").text(text);
+        $("#theme").toggle();
+        $("#mute").toggle();
+        $("#fullscreen").toggle();
+        $("#footer").toggle();
+        audio();
+        var color = window.getComputedStyle(document.body, null).getPropertyValue('background-color');
+        var id = window.setInterval(function(){
+            if(running){
+                color = color.split("(")[1].split(")")[0];
+                color = color.split(",");
+                var red = parseInt(color[0]);
+                var green = parseInt(color[1]);
+                var blue = parseInt(color[2]);
+                red += curInc[0];
+                green += curInc[1];
+                blue += curInc[2];
+                red = red.toString(16);
+                green = green.toString(16);
+                blue = blue.toString(16);
+                red = fixString(red);
+                green = fixString(green);
+                blue = fixString(blue);
+                document.body.style.backgroundColor = "#" + red + green + blue;
+                color = document.body.style.backgroundColor;
+                checkColoring(red, green, blue);
+            }
+            else{
+                window.clearInterval(id);
+            }
+        }, curSpeed);
+    });
+    $("#theme").click(function(){
+        theme = (theme+1)%5;
+        audioTrack = tracks[theme];
+        curInc = incs[theme];
+        curSpeed = speeds[theme];
+        document.body.style.backgroundColor = colors[theme];
+        $("#color").text(themes[theme]);
+    });
+    $("#mute").click(function(){
+        var text = toggleMute();
+        $("#mute").text(text);
+    });
+    $("#fullscreen").click(function(){
+        if(window.innerHeight == screen.height){
+            document.webkitExitFullscreen();
+        }
+        else{
+            document.body.webkitRequestFullScreen();
+        }
+    });
+});
